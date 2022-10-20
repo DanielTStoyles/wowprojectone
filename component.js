c189f5122653cec0ffc94bd03eeeb5d45c563ff5
DanielTStoyles <daniel_stoyles@live.ca>
2022-10-20 16:36:28 -0230

    make list items selectable return text content on click from list item to "choose class" paragraph remove bloat code in stylesheet, component.js, tableindex.html remove comments that are no longer necessary

--- component.js
+++ component.js
@@ -8,34 +8,29 @@ function dropMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// /* close dropdown menu if user clicks outside of it */
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+/* NEED TO FIGURE OUT WHAT IS WRITTEN INCORRECTLY IN THIS DROPDOWN FUNCTION BELOW
+- IT IS CLOSING THE MENU WHEN I CLICK ON LIST ITEMS, NEED IT TO ONLY CLOSE WHEN I CLICK 
+OUTSIDE THE MENU/BUTTON OR ONCE A LIST ITEM IS CLICKED */
+
+// // /* close dropdown menu if user clicks outside of it */
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     let dropdowns = document.getElementsByClassName("dropdown-content");
+//     let i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       let openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
 
 let image = document.createElement("img");
 image.src = "images/unholydk.png";
 document.querySelector(".dropbtn").appendChild(image);
 
-let inputSelect = document.getElementById("selection");
-inputSelect.addEventListener("mousedown", selection);
-
 function selection() {
   document.getElementById("selection").innerHTML =
-    getElementById("listItem").text;
+    document.getElementById("listItem").innerHTML;
 }
-
-/* creates onclick event for list items */
-
-// /* MAKE OPTIONS SELECTABLE THROUGH POTENTIALLY ON CLICK EVENTS INSIDE THE DROPDOWN MENU
-// (STOP THINKING ABOUT THE DAMN IMAGES)
-// CREATE AN OBJECT THAT IS REUSABLE FOR ALL LIST ITETMS*/
