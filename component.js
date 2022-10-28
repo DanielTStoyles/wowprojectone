643822b554d0e7310eba00037b5657727e93f724
DanielTStoyles <daniel_stoyles@live.ca>
2022-10-28 20:47:03 -0230

    Styling and fucntional work some dropdown content styling done change return of <li> selection in #selectionText to exclude text of <li>

--- component.js
+++ component.js
@@ -1,38 +1,42 @@
-/**
- * /* when the user clicks on the bbutton toggle showing and hiding the dropdown content
- *
- * @format
- */
+/** @format */
 
 function dropMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-/* NEED TO FIGURE OUT WHAT IS WRITTEN INCORRECTLY IN THIS DROPDOWN FUNCTION BELOW
-- IT IS CLOSING THE MENU WHEN I CLICK ON LIST ITEMS, NEED IT TO ONLY CLOSE WHEN I CLICK 
-OUTSIDE THE MENU/BUTTON OR ONCE A LIST ITEM IS CLICKED */
+// /* close dropdown menu if user clicks outside of it */
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
 
-// // /* close dropdown menu if user clicks outside of it */
-// window.onclick = function (event) {
-//   if (!event.target.matches(".dropbtn")) {
-//     let dropdowns = document.getElementsByClassName("dropdown-content");
-//     let i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       let openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains("show")) {
-//         openDropdown.classList.remove("show");
-//       }
-//     }
-//   }
-// };
-
-let image = document.createElement("img");
-image.src = "images/unholydk.png";
-document.querySelector(".dropbtn").appendChild(image);
-
-function selection() {
-  document.getElementById("selection").innerHTML =
-    document.getElementById("listItem").innerHTML;
+function selection(id) {
+  document.getElementById("selectionText").innerHTML =
+    document.getElementById(id).innerHTML;
+  imgTest(id);
 }
 
-//we good?
+function imgTest() {
+  let imageTest = document.createElement("IMG");
+  imageTest.setAttribute("src", listImage.getAttribute("src"));
+  imageTest.setAttribute("width", "40");
+  imageTest.setAttribute("height", "40");
+  removeText();
+  document.getElementById("selectionText").appendChild(imageTest);
+}
+
+function removeText() {
+  originalText = document.getElementById("selectionText").textContent;
+  newText = originalText.replace(originalText, "");
+  document.getElementById("selectionText").textContent = newText;
+}
+
+let listImage = document.getElementById("listImage");
