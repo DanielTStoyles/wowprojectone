0c801d6aeaaad7f6caa99ec2ac829bc82e4634bf
DanielTStoyles <daniel_stoyles@live.ca>
2022-11-10 15:24:43 -0330

    push

--- component.js
+++ component.js
@@ -26,57 +26,64 @@ function selection(id) {
 function playerForms() {
   let playerForm = document.createElement("form");
   playerForm.setAttribute("method", "post");
-  playerForm.setAttribute("action", "submit.php");
+  playerForm.setAttribute("id", "playerForm");
 
   let pN = document.createElement("input");
+  pN.setAttribute("id", "name");
   pN.setAttribute("type", "text");
-  pN.setAttribute("playerName", "PlayerName");
+  pN.setAttribute("playerName", "namePlayer");
   pN.setAttribute("placeholder", "Player Name");
 
-  let s = document.createElement("input");
-  s.setAttribute("type", "submit");
-  s.setAttribute("value", "Submit");
+  let classSpec = document.createElement("input");
+  classSpec.setAttribute("id", "classSpec");
+  classSpec.setAttribute("type", "text");
+  classSpec.setAttribute("classAndSpec", "ClassAndSpec");
+  classSpec.setAttribute("placeholder", "Class/Spec");
+
+  let dataBtn = document.createElement("button");
+  dataBtn.setAttribute("type", "submit");
+  dataBtn.setAttribute("id", "dataBtn"), (dataBtn.textContent = "Save");
 
   playerForm.appendChild(pN);
-  playerForm.appendChild(s);
-  document.getElementById("here").appendChild(playerForm);
+  playerForm.appendChild(classSpec);
+  document.getElementById("tdone").appendChild(playerForm);
+  document.getElementById("tdone").appendChild(dataBtn);
+
+  let form = document.getElementById("playerForm");
+  dataBtn.addEventListener("click", addPlayer);
+  dataBtn.addEventListener("click", form.remove());
+  // dataBtn.addEventListener("click", formHide());
 }
 
-// function hide() {
-//   let selectionItem = document.getElementById("selectionText");
-//   if (selectionItem.dataset.selection >= "0") {
-//     specList.style.display = "none";
+let playerData = [];
+
+// function formHide() {
+//   document
+//     .getElementById("playerForm")
+//     .parentElement.removeChild(document.getElementById("playerForm"));
+//   document.getElementById("dataBtn").remove();
+// }
+
+const addPlayer = (ev) => {
+  let player = {
+    id: Date.now(),
+    name: document.getElementById("name").value,
+    classSpec: document.getElementById("classSpec").value,
+  };
+  playerData.push(player);
+  console.warn("added", { playerData });
+  localStorage.setItem("playerList", JSON.stringify(playerData));
+};
+
+// const fileSystem=require("browserify-fs")
+// playerData.push(player);
+// const data = JSON.stringify(playerData);
+// document.querySelector("form").reset();
+// writeFile("./playerData.json", data, (err) => {
+//   if (err) {
+//     console.log("Error writing file", err);
 //   } else {
-//     specList.style.display = "block";
+//     console.log("JSON data is written to the file successfully");
 //   }
-// }
-
-// let lis = document.querySelectorAll("li");
-
-// let lis_array = Array.from(lis);
-
-// lis_array.forEach(function (elem) {
-//   elem.addEventListener("click", function (e) {
-//     if (elem.dataset.selection >= "0") {
-//       elem.style.display = "none";
-//     }
-//   });
 // });
-
-// function imgTest() {
-//   let imageTest = document.createElement("IMG");
-//   imageTest.addEventListener("click", function (e) {
-//     console.log(e.target.src);
-//   });
-//   imageTest.setAttribute("src", e.target.src);
-//   imageTest.setAttribute("width", "40");
-//   imageTest.setAttribute("height", "40");
-//   document.getElementById("tester").appendChild(imageTest);
-// }
-
-// function removeText() {
-//   originalText = document.getElementById("tester").textContent;
-//   newText = originalText.replace(originalText, "");
-//   document.getElementById("tester").textContent = newText;
-
-// }
+// };
