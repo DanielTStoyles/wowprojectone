e7b1a3c320913753003b6a15a02bc243efc0827c
DanielTStoyles <daniel_stoyles@live.ca>
2022-12-14 19:47:08 -0330

    IN PROGRESS Dynamically generate dropdown menu button in each table cell TO DO: add functionality for each button to display dropdown menu onclick requires creation of menu in javascript, maybe use HTML template and cloning?

--- component.js
+++ component.js
@@ -149,20 +149,34 @@ function genTbl2() {
   table.setAttribute("id", Date.now());
   table.setAttribute("class", "groupTable");
 
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 5; i++) {
     const row = document.createElement("tr");
 
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 1; j++) {
       const cell = document.createElement("td");
-      const cellText = document.createTextNode(`text ${i}, text ${j}`);
-      cell.appendChild(cellText);
+      // const cellText = document.createTextNode(`text ${i}, text ${j}`);
+      // cell.appendChild(cellText);
+      let button = document.createElement("button");
+      button.setAttribute("id", "dropBtn");
+      button.setAttribute("class", "dropbtn");
+      button.textContent = "Class/Spec";
+      button.addEventListener("click", () => {
+        document.getElementById("myDropdown").classList.toggle("show");
+      });
+      cell.appendChild(button);
       row.appendChild(cell);
-    }
-    tableBody.appendChild(row);
-    table.appendChild(tableBody);
-    tContainer.appendChild(table);
-    for (let i = 0; i < 1; i++) {
-      genTHead();
+      tableBody.appendChild(row);
     }
   }
+  let tHead = table.createTHead();
+  let tRow = tHead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    tRow.appendChild(th);
+  }
+  table.appendChild(tRow);
+  table.appendChild(tableBody);
+  tContainer.appendChild(table);
 }
